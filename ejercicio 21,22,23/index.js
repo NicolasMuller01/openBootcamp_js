5ff0110334b2ca39277369a639529335e99baabe
Nicolas Müller <nicolasmatiasmuller19@gmail.com>
2022-04-20 15:15:29 -0300

    (xiv)

---
+++ ejercicio 21,22,23/index.js
@@ -0,0 +1,26 @@
+// const div = document.querySelector(".rectangulo");
+// const cuadrado = document.querySelector(".cuadrado");
+// const circulo = document.querySelector(".circulo");
+// function ColorCode() {
+//     let makingColorCode = '0123456789ABCDEF';
+//     let finalCode = '#';
+//     for (let counter = 0; counter < 6; counter++) {
+//        finalCode =finalCode+ makingColorCode[Math.floor(Math.random() * 16)];
+//     }
+//     return finalCode;
+// }
+
+// div.addEventListener("mouseover",()=>{
+//     div.style.backgroundColor="white";
+// })
+// div.addEventListener("mouseout",()=>{
+//     div.style.backgroundColor=ColorCode();
+// })
+
+// cuadrado.addEventListener("click",()=>{
+//     cuadrado.style.backgroundColor=ColorCode();
+// })
+
+// cuadrado.addEventListener("dblclick",()=>{
+//     cuadrado.className="circulo";
+// })
